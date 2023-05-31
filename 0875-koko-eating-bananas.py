1020f83f2fe3cb8ff027fc4d49720d0129605aaf
Filipo Marcellino <66431048+filipomarcellino@users.noreply.github.com>
2023-05-30 23:53:04 -0700

    Update 0875-koko-eating-bananas.py

--- 0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas.py
@@ -15,4 +15,4 @@ class Solution:
                 l = mid + 1
         return k
 #data structure used: binary search
-#what's unique about this problem is to figure out the maximum possible value of k, which is the max number of banana in a pile. See logically speaking, if we take the largest pile, we can always eat other piles in an hour. Now that we have a range of numbers for k, we can apply binary search to find the minimum value of k so that koko can eat as slow as possible while being able to finish before the person comes back
+#what's unique about this problem is to figure out the maximum possible value of k, which is the max number of banana in a pile. See logically speaking, if we take the largest pile, we can always eat other piles in an hour. Now that we have a range of numbers for k, we can apply binary search to find the minimum value of k so that koko can eat as slow as possible while being able to finish before the person comes back. One other thing about this is to set the value k to the max range. This is because we want to keep replacing k with smaller and smaller possible values until we get the most minimum.
