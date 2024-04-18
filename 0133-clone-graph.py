49dd5b3bd24196b2b7de28618875c215b4898fa5
Filipo Marcellino <66431048+filipomarcellino@users.noreply.github.com>
2024-04-18 12:26:27 -0700

    update 0133-clone-graph.py

--- 0133-clone-graph.py
+++ 0133-clone-graph.py
@@ -37,7 +37,37 @@ class Solution:
         return oldNew[node]
                 
 """
-This is the iterative BFS solution
+This is the iterative BFS solution (to change to iterative DFS, just change the queue to a stack)
 Time complexity: O(n) where n is the number of nodes + number of edges
 Space complexity: O(n * n-1) because each node can have at most n-1 neighbours
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+from typing import Optional
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        oldNew = {}
+        def dfs(node):
+            if node in oldNew:
+                return oldNew[node]
+            
+            nodeCopy = Node(node.val)
+            oldNew[node] = nodeCopy
+ 
+            for neighbor in node.neighbors:
+                nodeCopy.neighbors.append(dfs(neighbor))
+            return nodeCopy
+        if not node:
+            return None
+        return dfs(node)
+
+"""
+recursive dfs solution
 """
