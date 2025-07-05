8f1be387c210ed4479aa7904a1c132673d31f8a5
Filipo Marcellino <66431048+filipomarcellino@users.noreply.github.com>
2025-07-05 10:58:10 -0700

    Update 0033-search-in-rotated-sorted-array.py

--- 0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array.py
@@ -1,77 +1,44 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l = 0
-        r = len(nums) - 1
-        while (l <= r):
-            m = (l + r) // 2
-            if nums[m] == target:
-                return m
-            
-            # left array is sorted
-            if nums[m] >= nums[l]:
-                if nums[l] <= target < nums[m]:
-                    r = m -1
+        def binarySearch(l, r):
+            while l <= r:
+                m = l + (r - l) // 2
+                if nums[m] == target:
+                    return m
+                elif nums[m] > target:
+                    r = m - 1
                 else:
                     l = m + 1
-            # right array is sorted
-            else:
-                if nums[m] < target <= nums[r]:
+            return -1
+            
+        def findPivot():
+            if nums[0] <= nums[-1]:
+                return 0
+            l = 0
+            r = len(nums) - 1
+            while l <= r:
+                m = l + (r - l) // 2
+                if m < len(nums) - 1 and nums[m] > nums[m + 1]:
+                    return m + 1
+                elif nums[m] > nums[-1]:
                     l = m + 1
                 else:
                     r = m - 1
-        return -1
-                
-#         def binarySearch(l, r):
-#             while(l <= r):
-#                 m = (l + r) // 2
-#                 if nums[m] == target:
-#                     return m
-#                 elif nums[m] > target:
-#                     r = m - 1
-#                 else:
-#                     l = m + 1
-#             return -1
-        
-#         l = 0
-#         r = len(nums) - 1
-#         pivot = -1
-#         # array rotated
-#         if nums[l] > nums[r]: 
-#             # find a pivot using binary search
-#             while(l <= r):
-#                 m = (l + r) // 2
-#                 if nums[m] > nums[m + 1]:
-#                     pivot = m
-#                     break
-#                 elif nums[m] < nums[l]:
-#                     r = m - 1
-#                 else:
-#                     l = m + 1
-                    
-#         if pivot != -1:
-#             l = 0
-#             r = pivot
-#             # do binary search on left array
-#             left = binarySearch(l, r)
-#             if left != -1:
-#                 return left
-            
-#             l = pivot + 1
-#             r = len(nums) - 1
-#             # do binary search on right array
-#             right = binarySearch(l, r)
-#             return right
-            
-#         else:
-#             # do regular binary search
-#             l = 0
-#             r = len(nums) - 1
-#             # do binary search
-#             return binarySearch(l, r)
 
+        # find pivot
+        pivotIdx = findPivot()
+        # perform binary search left
+        resultLeft = binarySearch(0, pivotIdx - 1)
+        if resultLeft != -1:
+            return resultLeft
+        # perform binary search right
+        return binarySearch(pivotIdx, len(nums) - 1)
 '''
 The uncommented solution is very much simpler, but the commented one is personally more intuitive for me. Both have an O(log n) time complexity
 the thing to remember is you have to figure out the edge cases like what if m == l? If there is a comparison that is out of the usual, like
 nums[m] >= nums[l], you have to think about the edge cases.
+
+edit (Jul 5th 2025):
+saved the most intuitive solution. Find pivot using a binary search
 '''
     
